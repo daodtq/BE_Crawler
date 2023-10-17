3368aec5a09b0418807c62871eff3d666e53eb1f
pain <trong.quang.dao@gmail.com>
2023-10-17 08:27:31 +0700

    update

--- api/models/Sheet.js
+++ api/models/Sheet.js
@@ -9,7 +9,7 @@ module.exports = {
     attributes: {
       id: { type: "string" },
       data: {type: "json"},
-      valuedate: {type: "json"},
+      valuedate: {type: "string"},
       name: {type: "string"}
     },
   };
