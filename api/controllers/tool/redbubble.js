7adb832e282053a4c7b7ead5fceaa050a2ff1164
daodtq <trong.quang.dao@gmail.com>
2023-11-30 09:32:10 +0700

    u

--- api/controllers/tool/redbubble.js
+++ api/controllers/tool/redbubble.js
@@ -84,8 +84,9 @@ module.exports = {
         let j = 0
         const fetchListingData = async () => {
             if (type == "link") {
-                const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
+                const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'], waitUntil: 'networkidle2', timeout: 60000 });
                 const page = await browser.newPage();
+                await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
 
                 try {
                     for (let i = from; i <= end; i++) {
